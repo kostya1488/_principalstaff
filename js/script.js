497560b40799b8e6d01379806d63cbca5ddd9f85
kostya <matvienkoigor956@gmail.com>
2020-11-29 17:22:04 +0200

    created card

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
+// ---------------------------------------------------------------
+
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+// ---------------------------------------------------------------
+
+$(document).ready(function() {
+    $('.munu_button').click(function(e) {
+        e.preventDefault();
+        $('.header').toggleClass('menu_opened');
+        $('.body').css('overflow', 'hidden');
+    });
+    $('.card').click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('flipped');
+    });
+
 });
